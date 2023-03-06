8b0e8f52437d7c8c181691f4b99a89f31c8c694f
BuildTools <unconfigured@null.spigotmc.org>
2023-03-06 23:11:00 +0100

    deploy task to write plugin jar to spigot fatJar fix to make kotlin work Convert all items dropped from a hopper to a dropper to diamond

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,10 @@
+import org.jetbrains.kotlin.gradle.plugin.mpp.pm20.util.archivesName
 import org.jetbrains.kotlin.ir.backend.js.compile
+import java.util.Properties
 
 plugins {
     kotlin("jvm") version "1.8.0"
+    `java-library`
 }
 
 group = "org.example"
@@ -23,4 +26,25 @@ tasks.test {
 
 kotlin {
     jvmToolchain(17)
+}
+
+val fatJar = task("fatJar", type = Jar::class) {
+    baseName = "${project.name}-fat"
+    duplicatesStrategy = org.gradle.api.file.DuplicatesStrategy.EXCLUDE
+
+    from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
+    with(tasks.jar.get() as CopySpec)
+}
+
+tasks.register<Copy>("deploy") {
+    //Check out local.properties.example
+    val deployPath = Properties().let {
+        it.load(project.rootProject.file("local.properties").inputStream())
+        it.getProperty("deployTo", "./build")
+    }
+
+    dependsOn(tasks.getByName("build"))
+    from(layout.buildDirectory.dir("libs/"))
+    into(deployPath)
+    include("*fat*.jar")
 }
--- src/main/kotlin/com/jsogaard/minecplusplus/Listener.kt
+++ src/main/kotlin/com/jsogaard/minecplusplus/Listener.kt
@@ -1,14 +1,18 @@
 package com.jsogaard.minecplusplus
 
-import org.bukkit.Bukkit
+import org.bukkit.Material
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
-import org.bukkit.event.player.PlayerJoinEvent
+import org.bukkit.event.inventory.InventoryMoveItemEvent
+import org.bukkit.event.inventory.InventoryType
+import org.bukkit.inventory.ItemStack
 
 
 class PluginListener: Listener {
     @EventHandler
-    fun onPlayerJoin(event: PlayerJoinEvent?) {
-        Bukkit.broadcastMessage("Welcome to the server ${event?.player?.name}!")
+    fun onMoveInventoryEvent(event: InventoryMoveItemEvent) {
+        if(event.initiator.type == InventoryType.HOPPER && event.destination.type == InventoryType.DROPPER) {
+            event.item = ItemStack(Material.DIAMOND, 1)
+        }
     }
 }

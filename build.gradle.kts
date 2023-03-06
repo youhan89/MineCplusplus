70c8d37fe2d2c2523cade8ac80aaeb15b708271f
Youhan <johan.sogaard@gmail.com>
2023-03-06 19:48:15 +0100

    Added spigot api dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.ir.backend.js.compile
+
 plugins {
     kotlin("jvm") version "1.8.0"
     application
@@ -8,10 +10,12 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven(url="https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
 }
 
 dependencies {
     testImplementation(kotlin("test"))
+    compileOnly("org.spigotmc:spigot-api:1.19.3-R0.1-SNAPSHOT")
 }
 
 tasks.test {

dffc22a319e24e3ad580b8dac7476d172956d4e4
ade <ade@ade.se>
2023-05-18 20:38:03 +0200

    Found proper way of calculating remaining items after crafting. Should now have fixed all recipes with the remaining item problem (bottle, bucket)

--- src/main/kotlin/com/jsogaard/minecplusplus/crafting/CraftingResolver.kt
+++ src/main/kotlin/com/jsogaard/minecplusplus/crafting/CraftingResolver.kt
@@ -5,20 +5,22 @@ import org.bukkit.Server
 import org.bukkit.World
 import org.bukkit.inventory.ItemStack
 
-private val honeyBottlesToHoneyBlock = (1..4).map { Material.HONEY_BOTTLE }
-
 class CraftingResolver(private val server: Server) {
     fun craft(input: List<ItemStack?>, world: World): List<ItemStack>? {
         val result = server.getCraftingRecipe(input.toTypedArray() as Array<out ItemStack>, world)?.result
             ?: return null
 
-        return when (countMaterials(input)) {
-            honeyBottlesToHoneyBlock -> return listOf(ItemStack(Material.HONEY_BLOCK), ItemStack(Material.GLASS_BOTTLE, 4))
-            else -> listOf(result)
-        }
+        val remainingMaterials = filterMaterials(input).mapNotNull {
+            if (it.isItem) it.craftingRemainingItem
+            else null
+        }.groupingBy { it }
+            .eachCount()
+            .map { ItemStack(it.key, it.value) }
+
+        return listOf(result) + remainingMaterials
     }
 
-    private fun countMaterials(input: List<ItemStack?>): List<Material> {
+    private fun filterMaterials(input: List<ItemStack?>): List<Material> {
         return input.mapNotNull {
             when(it?.type) {
                 null, Material.AIR -> null
